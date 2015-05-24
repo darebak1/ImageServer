a40b1d165ac4035e8fdb1c1d3ff56cbd7f1edcb6
darebak1 <m.darebak@outlook.com>
2015-05-24 13:05:23 +0200

    start

---
+++ ImageServer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImageServer.Startup))]
+namespace ImageServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
